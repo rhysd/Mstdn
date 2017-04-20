0f449b8b2b870f89f8d49a949b6e043579e7d7f4
rhysd <lin90162@yahoo.co.jp>
2017-04-20 10:48:29 +0900

    show app crash with dialog and die

--- main/index.ts
+++ main/index.ts
@@ -1,4 +1,4 @@
-import {app} from 'electron';
+import {app, dialog} from 'electron';
 import log from './log';
 import startApp from './app';
 import loadConfig from './config';
@@ -10,7 +10,17 @@ app.removeAllListeners();
 const appReady = new Promise<void>(resolve => app.once('ready', resolve));
 
 process.on('unhandledRejection', (reason: string) => {
-    log.error('FATAL: Unhandled rejection! Reason:', reason);
+    const msg = 'FATAL: Unhandled rejection! Reason: ' + reason
+    appReady.then(() => {
+        dialog.showMessageBox({
+            type: 'error',
+            buttons: ['OK'],
+            message: msg,
+        }, () => {
+            qpp.quit();
+        })
+    });
+    log.error(msg);
 });
 
 app.on('will-quit', () => {
